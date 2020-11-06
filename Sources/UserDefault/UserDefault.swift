b8be80fed4d362a776cbb9cb2644a6b09d094370
Gienadij Mackiewicz <gienadijmackiewicz@gmail.com>
2020-11-06 09:01:12 +0100

    Add initial UserDeafault source code

--- Sources/UserDefault/UserDefault.swift
+++ Sources/UserDefault/UserDefault.swift
@@ -1,3 +1,55 @@
-struct UserDefault {
-    var text = "Hello, World!"
+import Foundation
+
+@available(iOS 2.0, OSX 10.0, tvOS 9.0, watchOS 2.0, *)
+@propertyWrapper
+public struct UserDefault<Value> {
+
+    // MARK: - Private properties
+
+    private let key: String
+    private let defaultValue: Value
+    private var userDefaults: UserDefaults
+
+    // MARK: - Public properties
+
+    public var wrappedValue: Value {
+        get {
+            let value = UserDefaults.standard.value(forKey: key) as? Value
+            return value ?? defaultValue
+        }
+        set {
+            if let optional = newValue as? AnyOptional, optional.isNil {
+                userDefaults.removeObject(forKey: key)
+            } else {
+                userDefaults.setValue(newValue, forKey: key)
+            }
+        }
+    }
+
+    // MARK: - Initialization
+
+    public init(_ key: String, defaultValue: Value, userDefaults: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.userDefaults = userDefaults
+        self.userDefaults.register(defaults: [key: defaultValue])
+    }
+}
+
+// MARK: - Convenience init for optionals
+
+public extension UserDefault where Value: ExpressibleByNilLiteral {
+    init(key: String) {
+        self.init(key, defaultValue: nil, userDefaults: .standard)
+    }
+}
+
+// MARK: - AnyOptional
+
+private protocol AnyOptional {
+    var isNil: Bool { get }
+}
+
+extension Optional: AnyOptional {
+    var isNil: Bool { self == nil }
 }
