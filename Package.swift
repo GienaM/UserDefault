b859fa9c9981144df0006e83ae2aa75f549cf754
Gienadij Mackiewicz <gienadijmackiewicz@gmail.com>
2020-11-29 22:47:19 +0100

    Update supported iOS version to 9 to silence Xcode 12 warning

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "UserDefault",
-    platforms: [.iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
     products: [
         .library(
             name: "UserDefault",
